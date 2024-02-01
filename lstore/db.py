3b5616528f949245caf64921bc744586d069fe50
aFishboy <samuelhoward914@gmail.com>
2024-01-31 20:24:23 -0800

    db.py mostly done

--- lstore/db.py
+++ lstore/db.py
@@ -19,20 +19,31 @@ class Database():
     :param num_columns: int     #Number of Columns: all columns are integer
     :param key: int             #Index of table key in columns
     """
+    # Create table may be done makes the table and adds it to self.tables and returns table when done
     def create_table(self, name, num_columns, key_index):
         table = Table(name, num_columns, key_index)
+        self.tables.append(table)  
         return table
 
     
     """
     # Deletes the specified table
     """
+    # searches through self.tables to find the specified table
     def drop_table(self, name):
-        pass
+        for table in self.tables:
+            if table.name == name:
+                self.tables.remove(table)
+                break
 
     
     """
     # Returns table with the passed name
     """
+    # searches through self.tables to find the specified table
+    
     def get_table(self, name):
-        pass
+        for table in self.tables:
+            if table.name == name:
+                return table
+        return None # should probably have some error handling added later here or at the function calling it

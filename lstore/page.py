f9a3712ad70769992dcbd5ca1e2f2bf8c76c3201
sam <samuelhoward914@gmail.com>
2024-02-03 19:07:42 -0800

    Page init and capacity done

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,17 @@
-
 class Page:
 
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = bytearray(4096) # 4096 bytes per page
+        self.RECORD_SIZE = 8  # 8 bytes for a 64-bit integer
+        self.MAX_RECORDS_PER_PAGE = len(self.data) // RECORD_SIZE # 4096 / 8 = 512
 
     def has_capacity(self):
-        pass
+        return self.num_records < self.MAX_RECORDS_PER_PAGE
 
+# Need to do
     def write(self, value):
         self.num_records += 1
         pass
 
+#Prob need a read function

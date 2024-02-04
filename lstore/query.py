3ebe89aaf5389f7a67616cd2c86bbbfdd8baf82d
[Tristan-Pham] <74520801+Tristan-Pham@users.noreply.github.com>
2024-02-03 17:33:31 -0800

    Update to table and query
    fixing insert implementation

--- lstore/query.py
+++ lstore/query.py
@@ -35,13 +35,8 @@ class Query:
             if len(columns) != self.table.num_columns:
                 print("Error: Number of values does not match the number columns.")
                 return False
-
-            new_record = {}
-            column_indices = list(range(self.table.num_columns))
-            for i in column_indices:
-                new_record[i] = columns[i]
-
-            #self.table.page_directory.append(new_record)  FIX ME
+            
+            self.table.data.append(columns) #does data need to be a dictionary
 
             print("Data inserted successfully!")
             return True

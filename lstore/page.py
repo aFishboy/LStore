e8d7dce78b2489bdcf8defbc2e58b535f7050463
aFishboy <samuelhoward914@gmail.com>
2024-02-07 19:06:26 -0800

    page validation and testing

--- lstore/page.py
+++ lstore/page.py
@@ -1,6 +1,5 @@
 from .config import *
 class Page:
-
     def __init__(self):
         self.num_records = 0
         self.data = bytearray(PAGE_DATA_SIZE) # 4096 bytes per page
@@ -14,18 +13,28 @@ class Page:
 
     def write(self, value_to_write):
         write_offset = self.current_offset()
+        if write_offset >= PAGE_DATA_SIZE:
+            raise IndexError("Attempted to write outside page")
         bytes_to_write = value_to_write.to_bytes(COLUMN_DATA_SIZE, byteorder='little')
         self.data[write_offset:write_offset + COLUMN_DATA_SIZE] = bytes_to_write
         self.num_records += 1
-        print(self.num_records)
 
     def read(self, read_offset) -> int:
         adjusted_read_offset = read_offset * COLUMN_DATA_SIZE
+        self.check_valid_offset(read_offset)
         bytes_read = self.data[adjusted_read_offset:adjusted_read_offset + COLUMN_DATA_SIZE]
         converted_data = int.from_bytes(bytes_read, byteorder='little')
         return converted_data
     
     def update_entry(self, offset, value_to_write):
-        adjusted_offset = offset * self.COL_DATA_SIZE
-        bytes_to_write = value_to_write.to_bytes(self.COL_DATA_SIZE, byteorder='little')
-        self.data[adjusted_offset:adjusted_offset + self.COL_DATA_SIZE] = bytes_to_write
+        self.check_valid_offset(offset)
+        adjusted_offset = offset * COLUMN_DATA_SIZE
+        bytes_to_write = value_to_write.to_bytes(COLUMN_DATA_SIZE, byteorder='little')
+        self.data[adjusted_offset:adjusted_offset + COLUMN_DATA_SIZE] = bytes_to_write
+
+    def check_valid_offset(self, offset):
+        adjusted_offset = offset * COLUMN_DATA_SIZE
+        if adjusted_offset + COLUMN_DATA_SIZE > len(self.data) or adjusted_offset < 0 :
+            raise IndexError("Attempted to access beyond the bounds of the data buffer")
+        if offset >= self.num_records:
+            raise IndexError("Attempted to access  an offset with no written data")
--- lstore/tests/test_page.py
+++ lstore/tests/test_page.py
@@ -18,6 +18,12 @@ class TestPage:
     def test_get_current_offset(self):
         page = Page()
         assert page.current_offset() == 0
+    
+    def test_get_current_offset_after_writes(self):
+        page = Page()
+        for i in range(10):
+            page.write(i)
+        assert page.current_offset() == 80
 
     # can handle writing to a full page
     def test_write_full_page(self):
@@ -25,12 +31,21 @@ class TestPage:
         for i in range(page.MAX_RECORDS_PER_PAGE):
             page.write(i)
         assert page.has_capacity() == False
+        assert page.current_offset() == 4096
 
     # can handle reading from an invalid offset
     def test_read_invalid_offset(self):
         page = Page()
+        for i in range(5):
+            page.write(i)
         with pytest.raises(IndexError):
-            page.read(1)
+            page.read(-1)
+        with pytest.raises(IndexError):
+            page.read(5)
+        for i in range(5, page.MAX_RECORDS_PER_PAGE):
+            page.write(i)
+        with pytest.raises(IndexError):
+            page.read(4096)
 
     # can handle updating an invalid offset
     def test_update_invalid_offset(self):
@@ -43,13 +58,18 @@ class TestPage:
         page = Page()
         page.write(10)
         assert page.num_records == 1
-        assert page.data[:COLUMN_DATA_SIZE] == b'\n\x00\x00\x00\x00\x00\x00\x00'
+        assert page.data[:COLUMN_DATA_SIZE * page.num_records] == b'\x0A\x00\x00\x00\x00\x00\x00\x00'
+        page.write(34550)
+        assert page.num_records == 2
+        assert page.data[COLUMN_DATA_SIZE * (page.num_records - 1):COLUMN_DATA_SIZE * page.num_records] == b'\xF6\x86\x00\x00\x00\x00\x00\x00'
 
     # can read a record from the page
     def test_read_record(self):
         page = Page()
         page.write(10)
         assert page.read(0) == 10
+        page.write(19923940)
+        assert page.read(1) == 19923940
 
     # can update an existing record on the page
     def test_update_record(self):

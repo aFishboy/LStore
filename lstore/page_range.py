92ef4e12e998c1a9cc0c9c4c94306f9863785616
Samuel Howard <srhoward@ucdavis.edu>
2024-02-10 19:24:55 -0800

    fixing pagerange add new record

--- lstore/page_range.py
+++ lstore/page_range.py
@@ -28,14 +28,17 @@ class PageRange:
         page = self.base_pages[self.last_base_page]
         # add the record values to pages
         for i in range(self.num_columns):
-            page[i].write(columns[i], self.last_base_offset)
+            page[i].write(columns[i])
             page[i].increment_record_count() # may need to be on the outside of for loop and only happen once idk
         # add RID
-        page[self.num_columns].write(rid, self.last_base_offset)
+        page[self.num_columns].write(rid)
+        page[self.num_columns].increment_record_count()
         # add indirection
-        page[self.num_columns + 1].write(0, self.last_base_offset)
+        page[self.num_columns + 1].write(0)
+        page[self.num_columns + 1].increment_record_count()
         # add schema encoding
-        page[self.num_columns + 2].write(0, self.last_base_offset)
+        page[self.num_columns + 2].write(0)
+        page[self.num_columns + 2].increment_record_count() # need to find better way to do this
 
         self.last_base_offset += 8
         return (self.last_base_page, self.last_base_offset - 8)

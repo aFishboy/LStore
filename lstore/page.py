295a58b769a150b91c0f252c05839a50beb4e55e
Ryan Fouzdar <ryanfouzdar@gmail.com>
2024-02-11 20:41:22 -0800

    Table done

--- lstore/page.py
+++ lstore/page.py
@@ -4,6 +4,8 @@ class Page:
         self.num_records = 0
         self.data = bytearray(PAGE_DATA_SIZE) # 4096 bytes per page
         self.MAX_RECORDS_PER_PAGE = PAGE_DATA_SIZE // COLUMN_DATA_SIZE # 4096 / 8 = 512
+        self.record_index = {}
+        self.RID = uuid.uuid4()
 
     def has_capacity(self) -> bool:
         return self.num_records < self.MAX_RECORDS_PER_PAGE
@@ -17,6 +19,8 @@ class Page:
             raise IndexError("Attempted to write outside page")
         bytes_to_write = value_to_write.to_bytes(COLUMN_DATA_SIZE, byteorder='little')
         self.data[write_offset:write_offset + COLUMN_DATA_SIZE] = bytes_to_write
+        self.record_index[self.num_records] = self.RID 
+        self.num_records += 1
 
     def read(self, read_offset) -> int:
         adjusted_read_offset = read_offset * COLUMN_DATA_SIZE
